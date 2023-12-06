7599849128e8376a6c37e95d59f402c6b7cbcf8c
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2023-12-06 00:49:46 -0500

    uwu

--- src/app/pages_Cliente/cliente/cliente.component.ts
+++ src/app/pages_Cliente/cliente/cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./cliente.component.scss']
 })
 export class ClienteComponent {
+
+  static validarCedula(control: FormControl): {[s: string]: boolean} | null {
+    const cedula = control.value;
+    if (cedula && cedula.length === 10 && !isNaN(Number(cedula))) {
+      return null; // Válido, devuelve null si es correcto
+    } else {
+      return { 'cedulaInvalida': true }; // Inválido, devuelve un objeto con la clave 'cedulaInvalida'
+    }
+  }
+
+    // Otras funciones de tu componente...
+    // Otras funciones de tu componente...
 
 }
