9fd0846f040e0a8e6ea269d8f6b7edfdc222ea7e
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2023-12-06 00:09:25 -0500

    ,jk,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteComponent } from './pages_Cliente/cliente/cliente.component';
+import { DocenteComponent } from './pages_Docente/docente/docente.component';
+import { CursoComponent } from './pages_Curso/curso/curso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/paginas/Inicio', pathMatch: 'full' },
+  {path: 'paginas/Clientes',component:ClienteComponent},
+  {path: 'paginas/Docentes', component:DocenteComponent},
+  {path: 'paginas/Cursos', component:CursoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './pages_Cliente/cliente/cliente.component';
+import { DocenteComponent } from './pages_Docente/docente/docente.component';
+import { CursoComponent } from './pages_Curso/curso/curso.component';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    DocenteComponent,
+    CursoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
