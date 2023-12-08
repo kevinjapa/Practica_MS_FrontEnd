5bc74086f630e4a17cc9f7a4d501d18dc1afeab9
pedroxavier1451 <pedro.xavier.orellana@gmail.com>
2023-12-07 23:25:44 -0500

    relacion entre curso y docente

--- src/app/domain/curso.ts
+++ src/app/domain/curso.ts
@@ -1,7 +1,7 @@
+import { Docente } from "./docente";
 
 export class Curso{
     nombre:String='';
     horas: String='';
-    docente:String='';
-    estudiante:String='';
+    docente!: Docente;
 }
--- src/app/pages_Curso/curso/curso.component.ts
+++ src/app/pages_Curso/curso/curso.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Curso } from 'src/app/domain/curso';
+import { Docente } from 'src/app/domain/docente';
 import { CursoService } from 'src/app/services/curso.service';
+import { DocenteService } from 'src/app/services/docente.service';
 
 @Component({
   selector: 'app-curso',
@@ -11,18 +13,28 @@ import { CursoService } from 'src/app/services/curso.service';
 export class CursoComponent {
 
   curso: Curso = new Curso();
+  docente: Docente = new Docente();
 
-  constructor(private cursosService: CursoService, private router: Router) {
+  constructor(private cursosService: CursoService, private docentesService: DocenteService, private router: Router) {
     let params = this.router.getCurrentNavigation()?.extras.queryParams;
     if(params){
       console.log(params)
       this.curso = new Curso()
       this.curso = params['curso']
     }
+
+    let params2 = this.router.getCurrentNavigation()?.extras.queryParams;
+      if(params2){
+        console.log(params2)
+        this.docente = new Docente()
+        this.docente = params2['docente']
+      }
+
   }
 
   guardar(){
     console.log(this.curso)
+    this.curso.docente=this.docente
     this.cursosService.save(this.curso).subscribe(data => {
       console.log("Resultado WS SAVE", data);
       this.router.navigate(['paginas/listCliente'])
